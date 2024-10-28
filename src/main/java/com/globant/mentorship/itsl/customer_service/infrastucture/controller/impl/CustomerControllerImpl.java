e8763556faf012725e19279039d0ff4c6a45f4ca
VictorOsorioG <victor.osorio@globant.com>
2024-10-28 07:59:10 -0500

    add injection of CustomerApplicationService and request mapping

--- src/main/java/com/globant/mentorship/itsl/customer_service/infrastucture/controller/impl/CustomerControllerImpl.java
+++ src/main/java/com/globant/mentorship/itsl/customer_service/infrastucture/controller/impl/CustomerControllerImpl.java
@@ -1,9 +1,12 @@
 package com.globant.mentorship.itsl.customer_service.infrastucture.controller.impl;
 
 import com.globant.mentorship.itsl.customer_service.application.dto.CustomerDto;
+import com.globant.mentorship.itsl.customer_service.application.service.CustomerApplicationService;
 import com.globant.mentorship.itsl.customer_service.infrastucture.controller.CustomerController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -13,8 +16,12 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/customer")
 public class CustomerControllerImpl implements CustomerController {
+
+    private final CustomerApplicationService customerApplicationService;
+
     @Override
-    public Mono<CustomerDto> getCustomerById(Long id) {
-        return null;
+    @GetMapping("/{id}")
+    public Mono<CustomerDto> getCustomerById(@PathVariable Long id) {
+        return customerApplicationService.getCustomer(id);
     }
 }
