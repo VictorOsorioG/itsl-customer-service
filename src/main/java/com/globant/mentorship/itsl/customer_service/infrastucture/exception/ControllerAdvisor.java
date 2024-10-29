09ca75e4147d7e37af5ac2d73c40f35d6ea31047
VictorOsorioG <victor.osorio@globant.com>
2024-10-29 15:50:20 -0500

    add controllerAdvisor log

--- src/main/java/com/globant/mentorship/itsl/customer_service/infrastucture/exception/ControllerAdvisor.java
+++ src/main/java/com/globant/mentorship/itsl/customer_service/infrastucture/exception/ControllerAdvisor.java
@@ -1,14 +1,19 @@
 package com.globant.mentorship.itsl.customer_service.infrastucture.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class ControllerAdvisor {
 
+    private final String LOG_PREFIX = "CONTROLLER ADVICE >>> Exception caught in";
+
     @ExceptionHandler(StandardException.class)
     public ResponseEntity<StandardError> handlerStandardException(StandardException standardException) {
+        log.info("{} handleStandardException : {}", LOG_PREFIX, standardException.getStandardError().getDescription());
         return ResponseEntity.status(standardException.getHttpStatus())
                 .body(standardException.getStandardError());
     }
